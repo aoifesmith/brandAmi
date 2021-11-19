a9b2182288b95117139b57b9e3b268fc4be24ee8
aoifesmith <aoifesmithy@hotmail.com>
2021-11-19 13:55:53 +0000

    FEAT: Add more eligibility questions to array with responses to continue through steps of eligibility questions

--- assets/js/eligible.js
+++ assets/js/eligible.js
@@ -32,12 +32,63 @@ let criteria_qs = {
     }
   },
 
-  "1_b": {
-    "question": 'Unfortunately you do not qualify for this application. YOu need to be over 18.',
+  "2_a": {
+    "question": "Q3/5: How many TikTok followers do you have?",
+    "answers": {
+      "a": "20k+ Followers",
+      "b": "0-20k Followers"
+    }
+  },
+  "3_a": {
+    "question": "Q4/5: Are you resident in Ireland?",
+    "answers": {
+      "a": "Yes",
+      "b": "No"
+    }
+  },
+  "4_a": {
+    "question": "Q5/5: Would you sell your soul to the devil?",
+    "answers": {
+      "a": "Of course I would!",
+      "b": "No"
+    }
   },
 
+  "5_a": {
+    "question": "Nice work. You've been approved!! We will be in contact with contracts within 5 working days."
+  },
 
+  "1_b": {
+    "question": "Unfortunately you do not qualify for this application. You need to be over 18.",
+  },
+  "2_b": {
+    "question": "Unfortunately you need to have over 5k IG followers to meet eligibility requirements.",
+  },
+  "3_b": {
+    "question": "Unfortunately you need to have over 20k TikTok followers to meet eligibility requirements.",
+  },
+  "4_b": {
+    "question": "Unfortunately you need to be a resident of Ireland to apply.",
+  },
+  "5_b": {
+    "question": "Unfortunately you need to sell your soul to the devil for this kind of work. Change your ways and come over to the dark side. We'll make it worth your will. Sure we promise don't we!!!",
+  },
 };
+
+// Continue link
+submit.addEventListener('mouseup', function () { //when a button on a pointing device is released while the pointer is located inside it
+  response = form.querySelectorAll('input[type=radio]:checked')[0].value; //a list of the document's elements that match the specified group of selectors.
+  if (response) {
+    criteria++; //increment or add 1 to
+    populateForm(criteria + '_' + response); //if a radio is checked populate form with the response
+    console.log("Application Worked!"); // Console log to make sure things are working
+  }
+});
+
+// Reset button - The reset() method resets the values of all elements in the form
+function resetForm() {
+  document.getElementById("appForm").reset();
+}
 
 // function to generate answers from criteria
 function populateForm(criteria) {
@@ -50,9 +101,10 @@ function populateForm(criteria) {
     }
   }
 
+
   form.querySelector('p').innerHTML = current_criteria.question; //write questions to the p tag 
   form.querySelector('fieldset').innerHTML = text; //write answers to the fieldset 
 
 }
 
-populateForm('start');//set the form at the beginning
+populateForm('start'); //set the form at the beginning
